9fb37e7e99df2f6e9f6eaaae155e158510690ed7
junara <tepada@yahoo.co.jp>
2016-02-27 12:40:04 +0000

    log in log out

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
   root to: 'static_pages#home'
+
   get 'signup' , to: 'users#new'
 
+  get 'login' , to: 'sessions#new'
+  post 'login' , to: 'sessions#create'
+  delete 'logout' , to: 'sessions#new'
+  
+
   resources :users
+  resources :sessions , only: [:new , :create , :destroy]
 
 end
