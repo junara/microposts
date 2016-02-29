bf494a2dfcb94be2cb6409b67f733b9dcccd0007
junara <tepada@yahoo.co.jp>
2016-02-29 06:04:02 +0000

    update profile

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,27 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  include SessionsHelper
+  before_action :logged_in_user, only: [:edit, :update , :show]
+  before_action :logged_in_current_user, only: [:edit, :update, :show]
+
+
+private
+    def logged_in_user
+        unless logged_in?
+          store_location
+          flash[:danger] = "Please log in."
+          redirect_to login_url
+        end
+    end
+    
+    def logged_in_current_user
+        logged_in_user # @current_userがnilの場合は以下は実行しない。
+        @current_user ||= User.find_by(id: session[:user_id]) #edit updateでは不要。showでは必要
+        unless @current_user.id.to_s == params[:id] #@current_userがnilの時はerrorが出る。
+          store_location
+          flash[:danger] = "Please log in."
+          redirect_to login_url
+        end
+        
+    end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
 
+  
   def show 
     @user = User.find(params[:id])
   end
@@ -18,9 +19,37 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id]) #モデルは大文字単数
+
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Update success"
+      render 'show'
+    else
+      flash[:success] = "Update failed"
+      render 'edit'
+    end
+  end
+
+
+
+
+
+  
+
+
   private
 
   def user_params
-    params.require(:user).permit( :name , :email , :password , :password_confirmation )
+    params.require(:user).permit( :name , :email , :password , :password_confirmation , :profile , :location)
   end
+  
+  
+
+
+
 end
