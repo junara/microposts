e4b2a261406f401112d05e89cd5fed3b18a4a8b7
junara <tepada@yahoo.co.jp>
2016-03-01 07:28:56 +0000

    move current user action from applicatio_controller to users_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,26 +2,13 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
   before_action :logged_in_user, only: [:edit, :update ]
-  before_action :logged_in_current_user, only: [:edit, :update]
-
 
 private
-    def logged_in_user
-        unless logged_in?
-          store_location
-          flash[:danger] = "Please log in."
-          redirect_to login_url
-        end
-    end
-    
-    def logged_in_current_user
-        logged_in_user # @current_userがnilの場合は以下は実行しない。
-        @current_user ||= User.find_by(id: session[:user_id]) #edit updateでは不要。showでは必要
-        unless @current_user.id.to_s == params[:id] #@current_userがnilの時はerrorが出る。
-          store_location
-          flash[:danger] = "Please log in."
-          redirect_to login_url
-        end
-        
-    end
+  def logged_in_user
+      unless logged_in?
+        store_location
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-
+  before_action :logged_in_current_user, only: [:edit, :update]
   
   def show 
     @user = User.find(params[:id])
@@ -35,21 +35,19 @@ class UsersController < ApplicationController
     end
   end
 
-
-
-
-
-  
-
-
   private
 
   def user_params
     params.require(:user).permit( :name , :email , :password , :password_confirmation , :profile , :location)
   end
   
-  
-
-
-
+  def logged_in_current_user
+      logged_in_user # @current_userがnilの場合は以下は実行しない。
+      @current_user ||= User.find_by(id: session[:user_id]) #edit updateでは不要。showでは必要
+      unless @current_user.id.to_s == params[:id] #@current_userがnilの時はerrorが出る。
+        store_location
+        flash[:danger] = "Invalid access !"
+        redirect_to root_url
+      end
+  end  
 end
